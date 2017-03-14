11357f25810abb14fd8a737c4cf845f76cc63ffc
michaelin1208 <michaelin1208@qq.com>
2017-03-14 18:04:03 +0800

    day1.1

---
+++ ChargeApp/SettingTabBarItemController.swift
@@ -0,0 +1,24 @@
+//
+//  SettingTabBarItemController.swift
+//  ChargeApp
+//
+//  Created by Michaelin on 2017/3/14.
+//  Copyright © 2017年 Michaelin. All rights reserved.
+//
+
+import UIKit
+
+class SettingTabBarItemController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
